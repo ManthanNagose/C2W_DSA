4a5ef22259cc1d06845a4901c33e40f9ae6c30cd
Manthan-Nagose <nagosemanthan@gmail.com>
2026-03-14 19:07:54 +0530

    File 1 is done

--- 02-Time Complexity/Lecture 1/CountFactor.java
+++ 02-Time Complexity/Lecture 1/CountFactor.java
@@ -1,3 +1,20 @@
+// 100 iterations that is N iterations
+
+class Factor{
+    int countFactor(int num){
+        int count =0; 
+        for(int i=1; i<=num; i++){
+            if(num % i == 0)
+                count ++;
+        }
+        return count;
+    }
+}
+
 public class CountFactor {
-    
+    public static void main(String[] args) {
+        Factor obj = new Factor();
+        int factorCount = obj.countFactor(100);
+        System.out.println("Taotal factor are : " + factorCount);    
+    }
 }
