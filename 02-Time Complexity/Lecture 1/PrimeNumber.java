32d2da8e62b986d3574489a2525ba2bc28453f51
Manthan-Nagose <nagosemanthan@gmail.com>
2026-03-15 02:22:10 +0530

    Prime Number file added

--- 02-Time Complexity/Lecture 1/PrimeNumber.java
+++ 02-Time Complexity/Lecture 1/PrimeNumber.java
@@ -1,4 +1,32 @@
+// Optimized code for checking Prime Number
 
 public class PrimeNumber {
-    
+    static int countFactor(int num){
+        int count = 0;
+
+        for(int i=1; i*i <= num; i++){
+            if(num % i == 0){
+                if(i == num/i)
+                    count++;
+                else{
+                    count += 2;
+                }
+            }
+        }
+        return count;
+    }
+
+    static boolean primeCheck(int num){
+        if(countFactor(num) == 2){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(primeCheck(13));
+        System.out.println(primeCheck(20));
+        System.out.println(primeCheck(17));
+    }
 }
